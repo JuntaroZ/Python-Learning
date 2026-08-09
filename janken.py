1ad0b1fa52749a6fb61346bd3b22ef3f8befd7ff
JuntaroZ <junmegan9863@gmail.com>
2026-08-09 14:52:58 +0900

    Lesson 4 - エラー処理を追加する

--- janken.py
+++ janken.py
@@ -3,6 +3,13 @@ janken_table = ["グー", "チョキ", "パー"]
 com_answer = random.randint(0, 2)
 print("Com =", janken_table[com_answer])
 
-# グー、チョキ、パーを数字で入力してもらう
-your_answer = int( input("0.グー、1.チョキ、2.パーのいずれか数字を半角で入力してください：") )
-print("You =", janken_table[your_answer])
+while True:
+    try:
+        # グー、チョキ、パーを数字で入力してもらう
+        your_answer = int( input("0.グー、1.チョキ、2.パーのいずれか数字を半角で入力してください：") )
+        print("You =", janken_table[your_answer])
+        break
+    except:
+        print("半角数字(0,1,2)を入力してください。")
+        continue
+
