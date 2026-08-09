1faabc42799ff966bf3c7ecf2f34ac0ccf0f8ce8
JuntaroZ <junmegan9863@gmail.com>
2026-08-09 13:57:42 +0900

    Lesson 3 - 自分のジャンケンを入力する

--- janken.py
+++ janken.py
@@ -2,3 +2,7 @@ import random
 janken_table = ["グー", "チョキ", "パー"]
 com_answer = random.randint(0, 2)
 print("Com =", janken_table[com_answer])
+
+# グー、チョキ、パーを数字で入力してもらう
+your_answer = int( input("0.グー、1.チョキ、2.パーのいずれか数字を半角で入力してください：") )
+print("You =", janken_table[your_answer])
