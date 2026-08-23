ab8b1bc2bfaca0fe523e61ac15096ac7a64683d0
JuntaroZ <junmegan9863@gmail.com>
2026-08-24 08:43:35 +0900

    Update

--- excel_translate.py
+++ excel_translate.py
@@ -16,15 +16,17 @@ index = 2 # A2から開始
 while True: # 無限ループ
     # A列の文章を読み取る
     translate_text = sheet[f"A{index}"].value
+    # A列が空白の場合はループを抜ける
     if translate_text is None or translate_text == "":
         break
-
+    # 翻訳を実行する
     response = client.models.generate_content(
         model = "gemini-3.5-flash-lite",
         contents = SOURCE_LANG + "を" + TARGET_LANG + "に翻訳（最適な結果を１つだけ返してください）：" + translate_text
     )
     # B列に結果を書き込む
     sheet[f"B{index}"].value = response.text
+    # 次の行に移動する
     index += 1
 
 book.save("./excel/translate.xlsx")
