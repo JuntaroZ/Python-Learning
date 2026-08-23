86150cd69c07f3302a115258bbda2adf35d26f73
JuntaroZ <junmegan9863@gmail.com>
2026-08-23 11:11:24 +0900

    Update

--- excel_translate.py
+++ excel_translate.py
@@ -5,19 +5,26 @@ from google import genai
 book = openpyxl.load_workbook("./excel/translate.xlsx")
 sheet = book.active
 
+source_lang = sheet["A1"].value # 翻訳元言語
+target_lang = sheet["B1"].value # 翻訳先言語
+
+print("翻訳元言語：" + source_lang  + "、翻訳先言語：" + target_lang)
+
 client = genai.Client()
 
-# A2～A11を順番に処理
-for row in range(2, 11):
-
-    # A列の日本語を読み取る
-    japanese_text = sheet.cell(row=row, column=1).value
+index = 2 # A2から開始
+while True: # 無限ループ
+    # A列の文章を読み取る
+    translate_text = sheet[f"A{index}"].value
+    if translate_text is None or translate_text == "":
+        break
 
     response = client.models.generate_content(
-        model="gemini-3.5-flash-lite",
-        contents="日本語を英語に翻訳（英語文１つだけ返してください）：" + japanese_text
+        model = "gemini-3.5-flash-lite",
+        contents = source_lang + "を" + target_lang + "に翻訳（最適な結果を１つだけ返してください）：" + translate_text
     )
-    # エクセルに書き込む
-    sheet.cell(row=row, column=2).value = response.text
+    # B列に結果を書き込む
+    sheet[f"B{index}"].value = response.text
+    index += 1
 
 book.save("./excel/translate.xlsx")
